d9821440722e02b7f3f68eb380a00c303bbdbf64
Meharjeet Singh <meharjeet_singh@sfu.ca>
2024-03-13 15:31:08 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require("express");
-const PORT = process.env.PORT || 8080;
 const app = express();
 const setupInfoRoute = require("./routes/info");
+const cors = require("cors");
 const setupInfopostRoute = require("./routes/info-post");
+const PORT = process.env.PORT || 8100;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to the Profile Service of Learn Sphere!",
-  });
+	res.json({
+		message: "Welcome to the Profile Service of Learn Sphere!",
+	});
 });
 
 setupInfoRoute(app);
 setupInfopostRoute(app);
 
 app.listen(PORT, () => {
-  console.log(`Profile Service is running on port: ${PORT}`);
+	console.log(`Profile Service is running on port: ${PORT}`);
 });
